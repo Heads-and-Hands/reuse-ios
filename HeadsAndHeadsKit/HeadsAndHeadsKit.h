40c562f41a3d23cb7a405a33d4cd5a054d6d4bb0
Sergey Gamayunov <sergey.gamayunov.87@gmail.com>
2018-07-09 16:33:47 +0300

    Initial Commit

---
+++ HeadsAndHeadsKit/HeadsAndHeadsKit.h
@@ -0,0 +1,19 @@
+//
+//  HeadsAndHeadsKit.h
+//  HeadsAndHeadsKit
+//
+//  Created by Sergey on 09/07/2018.
+//  Copyright © 2018 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeadsAndHeadsKit.
+FOUNDATION_EXPORT double HeadsAndHeadsKitVersionNumber;
+
+//! Project version string for HeadsAndHeadsKit.
+FOUNDATION_EXPORT const unsigned char HeadsAndHeadsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeadsAndHeadsKit/PublicHeader.h>
+
+
