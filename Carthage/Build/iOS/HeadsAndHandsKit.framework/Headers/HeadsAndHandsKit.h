fa93b4d7338fc6b44585d6f104c8b4a63ca80729
Sergey Gamayunov <sergey.gamayunov.87@gmail.com>
2018-07-10 08:27:17 +0300

    Carthage - 2

---
+++ Carthage/Build/iOS/HeadsAndHandsKit.framework/Headers/HeadsAndHandsKit.h
@@ -0,0 +1,19 @@
+//
+//  HeadsAndHandsKit.h
+//  HeadsAndHandsKit
+//
+//  Created by Sergey on 09/07/2018.
+//  Copyright © 2018 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeadsAndHeadsKit.
+FOUNDATION_EXPORT double HeadsAndHandsKitVersionNumber;
+
+//! Project version string for HeadsAndHandsKit.
+FOUNDATION_EXPORT const unsigned char HeadsAndHandsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeadsAndHeadsKit/PublicHeader.h>
+
+
