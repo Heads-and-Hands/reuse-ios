7e3e818537e94d37a4c475c45b70fc9cafc725a9
Sergey Gamayunov <sergey.gamayunov.87@gmail.com>
2018-07-11 10:13:38 +0300

    Adds login

--- HeadsAndHandsKit/HeadsAndHandsKit.swift
+++ HeadsAndHandsKit/HeadsAndHandsKit.swift
@@ -13,3 +13,69 @@ public enum HeadsAndHeadsKit {
         print("Hello World")
     }
 }
+
+public class Reuse {
+    static var shared: Reuse!
+    private let config: ReuseConfiguration
+    private static let defaults = UserDefaults.standard
+
+
+    public static func instantiate(with config: ReuseConfiguration) {
+        Reuse.shared = Reuse(config: config)
+    }
+
+    private init(config: ReuseConfiguration) {
+        self.config = config
+    }
+
+    var session: URLSession {
+        return URLSession(configuration: .default)
+    }
+
+    var baseURL: URL {
+        return config.url
+    }
+}
+
+public class ReuseConfiguration {
+    let url: URL
+    public init(url: URL) {
+        self.url = url
+    }
+}
+
+protocol LoginViewControllerProtocol {
+    var login: String { get set }
+    var password: String { get set }
+
+    func logIn()
+}
+
+extension LoginViewControllerProtocol where Self: UIViewController {
+    func logIn(completion: @escaping (Bool) -> Void) {
+        let url = URL(fileURLWithPath: "/auth", relativeTo: Reuse.shared.baseURL)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        let params = [
+            "login": login,
+            "password": password
+        ]
+
+        request.httpBody = try! JSONSerialization.data(withJSONObject: params, options: [])
+
+        let task = Reuse.shared.session.dataTask(with: request) { data, response, error in
+            guard let response = response as? HTTPURLResponse else {
+                completion(false)
+                return
+            }
+
+            if response.statusCode == 200 {
+                completion(true)
+            }
+
+        }
+
+        task.resume()
+    }
+}
